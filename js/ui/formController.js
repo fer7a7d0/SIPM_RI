84fb0bacdfece1c74f553e63416798c089c6ae5c
Fernando <ingind.morf@gmail.com>
2026-03-15 13:39:23 -0600

    Actualizar formController.js

--- js/ui/formController.js
+++ js/ui/formController.js
@@ -99,7 +99,7 @@ const FormController = (() => {
             _statusEl.textContent = '';
             _statusEl.className   = 'form-status';
         }, 3500);
-        F.code.focus();
+    F.code.focus();
     }
 
     /* --- Sincronizar visibilidad del botón de descarga -------- */
